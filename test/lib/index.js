be1c71a941091c0862c9453e3c40fa9c59930368
mistadikay <koltsov.d@gmail.com>
2016-02-22 00:17:11 +0700

    :space_invader: refactor tests

--- test/lib/index.js
+++ test/lib/index.js
@@ -2,6 +2,13 @@ import assert from 'assert';
 
 import { buildClassName } from '../../lib/';
 
+function test(BEMJSON, className) {
+    assert.strictEqual(
+        buildClassName(BEMJSON),
+        className
+    );
+}
+
 describe('buildClassName', function() {
     it('is function', function() {
         assert(typeof buildClassName === 'function');
@@ -12,24 +19,24 @@ describe('buildClassName', function() {
     });
 
     it('empty class if no block and mix', function() {
-        assert(buildClassName({}) === '');
+        test({}, '');
     });
 
     it('empty class if no block and mix, but with className', function() {
-        assert(buildClassName({ className: 'lol' }) === 'lol');
+        test({ className: 'lol' }, 'lol');
     });
 
     describe('block', function() {
         it('simple', function() {
-            assert(buildClassName({ block: 'block' }) === 'block');
+            test({ block: 'block' }, 'block');
         });
 
         it('props.className + block', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block2',
                     className: 'block1'
-                }) === 'block1 block2'
+                }, 'block1 block2'
             );
         });
     });
@@ -38,46 +45,46 @@ describe('buildClassName', function() {
         describe('block', function() {
             it('block + mod', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         mods: {
                             mod: 'val'
                         }
-                    }) === 'block block_mod_val'
+                    }, 'block block_mod_val'
                 );
             });
 
             it('block + few mods', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         mods: {
                             mod1: 'val1',
                             mod2: 'val2'
                         }
-                    }) === 'block block_mod1_val1 block_mod2_val2'
+                    }, 'block block_mod1_val1 block_mod2_val2'
                 );
             });
 
             it('block + shorthand mod = true', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         mods: {
                             mod: true
                         }
-                    }) === 'block block_mod'
+                    }, 'block block_mod'
                 );
             });
 
             it('block + shorthand mod = false', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         mods: {
                             mod: false
                         }
-                    }) === 'block'
+                    }, 'block'
                 );
             });
         });
@@ -85,50 +92,50 @@ describe('buildClassName', function() {
         describe('elem', function() {
             it('block + elem + mod', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         elem: 'elem',
                         mods: {
                             mod: 'val'
                         }
-                    }) === 'block__elem block__elem_mod_val'
+                    }, 'block__elem block__elem_mod_val'
                 );
             });
 
             it('block + elem + few mods', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         elem: 'elem',
                         mods: {
                             mod1: 'val1',
                             mod2: 'val2'
                         }
-                    }) === 'block__elem block__elem_mod1_val1 block__elem_mod2_val2'
+                    }, 'block__elem block__elem_mod1_val1 block__elem_mod2_val2'
                 );
             });
 
             it('block + shorthand mod = true', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         elem: 'elem',
                         mods: {
                             mod: true
                         }
-                    }) === 'block__elem block__elem_mod'
+                    }, 'block__elem block__elem_mod'
                 );
             });
 
             it('block + shorthand mod = false', function() {
                 assert(
-                    buildClassName({
+                    {
                         block: 'block',
                         elem: 'elem',
                         mods: {
                             mod: false
                         }
-                    }) === 'block__elem'
+                    }, 'block__elem'
                 );
             });
         });
@@ -137,28 +144,28 @@ describe('buildClassName', function() {
     describe('mix', function() {
         it('mix without block', function() {
             assert(
-                buildClassName({
+                {
                     mix: {
                         block: 'block'
                     }
-                }) === 'block'
+                }, 'block'
             );
         });
 
         it('block + mix', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     mix: {
                         block: 'block2'
                     }
-                }) === 'block1 block2'
+                }, 'block1 block2'
             );
         });
 
         it('block + mods + mix', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     mods: {
                         mod: 'val'
@@ -166,25 +173,25 @@ describe('buildClassName', function() {
                     mix: {
                         block: 'block2'
                     }
-                }) === 'block1 block1_mod_val block2'
+                }, 'block1 block1_mod_val block2'
             );
         });
 
         it('block + elem + mix', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     elem: 'elem',
                     mix: {
                         block: 'block2'
                     }
-                }) === 'block1__elem block2'
+                }, 'block1__elem block2'
             );
         });
 
         it('block + elem + mods + mix', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     elem: 'elem',
                     mods: {
@@ -193,13 +200,29 @@ describe('buildClassName', function() {
                     mix: {
                         block: 'block2'
                     }
-                }) === 'block1__elem block1__elem_mod_val block2'
+                }, 'block1__elem block1__elem_mod_val block2'
+            );
+        });
+
+        it('block + elem + mods + mix + className', function() {
+            assert(
+                {
+                    block: 'block1',
+                    elem: 'elem',
+                    mods: {
+                        mod: 'val'
+                    },
+                    mix: {
+                        block: 'block2'
+                    },
+                    className: 'hello'
+                }, 'hello block1__elem block1__elem_mod_val block2'
             );
         });
 
         it('complex mix', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     mix: {
                         block: 'block2',
@@ -209,13 +232,13 @@ describe('buildClassName', function() {
                             mod2: 'val2'
                         }
                     }
-                }) === 'block1 block2__elem block2__elem_mod1_val1 block2__elem_mod2_val2'
+                }, 'block1 block2__elem block2__elem_mod1_val1 block2__elem_mod2_val2'
             );
         });
 
         it('multiple mixes', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     mix: [
                         {
@@ -225,13 +248,13 @@ describe('buildClassName', function() {
                             block: 'block3'
                         }
                     ]
-                }) === 'block1 block2 block3'
+                }, 'block1 block2 block3'
             );
         });
 
         it('multiple mixes with holes', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     mix: [
                         undefined,
@@ -240,13 +263,13 @@ describe('buildClassName', function() {
                         },
                         null
                     ]
-                }) === 'block1 block2'
+                }, 'block1 block2'
             );
         });
 
         it('recursive mixes', function() {
             assert(
-                buildClassName({
+                {
                     block: 'block1',
                     mix: {
                         block: 'block2',
@@ -254,7 +277,7 @@ describe('buildClassName', function() {
                             block: 'block3'
                         }
                     }
-                }) === 'block1 block2 block3'
+                }, 'block1 block2 block3'
             );
         });
     });
